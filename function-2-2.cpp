2a63a3d7087cb945bb70a487f43dbf54586bae2f
Amna Sajjad <a1824665@adelaide.edu.au>
2022-03-09 00:02:17 +1030

    q2hard

---
+++ function-2-2.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+# include <stdlib.h>
+# include <cmath>
+
+// function that converts from binary given as a array to decimal  
+
+int binary_to_number(int binary_digits[], int number_of_digits){
+
+     int decimal = 0;
+
+    for (int index = 0; binary_digits > 0; index++ ){
+        if ((binary_digits[index] % 10) == 1){
+            decimal = decimal + pow(2,index);
+        }
+        binary_digits[index] /= 10;
+    }
+ return decimal;
+}
---
+++ main-2-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+
+extern int binary_to_number(int binary_digits[], int number_of_digits);
+
+int main(){
+    int binary_digits[] = {1,0,0,1,0,0,1};
+    int number_of_digits = 7;
+
+    std::cout << binary_to_number(binary_digits,number_of_digits) << std::endl;
+}
