4287635d1dfa2b881f728c68dce1d89c278944c5
Amna Sajjad <a1824665@adelaide.edu.au>
2022-03-08 21:24:09 +1030

    q2easy

---
+++ function-1-2.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+// Function that determines if a 10x10 matrix is a identity matrix or not
+int identity(int array[10][10]){
+int matrix  = 1;
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            if (array[i][i] != 1 && array[i][j] != 0){
+                matrix = 0;
+                break;
+            }
+        }
+    }
+    return matrix;
+}
