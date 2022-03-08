9868f076227f4f1799d396eaec1dad63c73fe5ff
Amna Sajjad <a1824665@adelaide.edu.au>
2022-03-08 21:28:20 +1030

    q2easy

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -5,7 +5,7 @@ int identity(int array[10][10]){
 int matrix  = 1;
     for (int i = 0; i < 10; i++){
         for (int j = 0; j < 10; j++){
-            if (array[i][i] != 1 && array[i][j] != 0){
+            if (array[i][i] != 1 && array[j][j] != 1 && array[i][j] != 0){
                 matrix = 0;
                 break;
             }
