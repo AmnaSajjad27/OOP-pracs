1d9890d5cfe4d619ca6fd610a4a8c217b3f532e0
Amna Sajjad <a1824665@adelaide.edu.au>
2022-03-09 14:48:50 +1030

    q2hard

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -8,7 +8,7 @@ int binary_to_number(int binary_digits[], int number_of_digits){
 
      int decimal = 0;
 
-    for (int index = 0; binary_digits > 0; index++ ){
+    for (int index = 0; index < number_of_digits; index++ ){
         if ((binary_digits[index] % 10) == 1){
             decimal = decimal + pow(2,index);
         }
