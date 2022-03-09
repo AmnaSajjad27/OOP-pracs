7dc20d2c2f0e67f75afc8ce510794c0e634fd74f
Amna Sajjad <a1824665@adelaide.edu.au>
2022-03-09 14:20:48 +1030

    q4easy

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -11,8 +11,8 @@ void print_scaled_matrix(int array[3][3],int scale){
 
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
-               std::cout << array[i][j];
-               std::cout << " ";
+               std::cout << array[i][j]; 
+                std::cout << " ";
             }
             std::cout << "\n";
         }
