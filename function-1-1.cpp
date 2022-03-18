e99e28a377d1853998dae7bfa0f8f5d2a78b34f8
Amna Sajjad <a1824665@adelaide.edu.au>
2022-03-18 11:07:32 +1030

    q1pracexam

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,19 +1,17 @@
 # include <iostream> 
 
-using namespace std;
 
 // function that prints all the elements of the array
 void printer(int array[10][10]){
 // Two loops, one for rows and one for columns
 for (int i = 0; i < 10; i++){
     for (int j = 0; j < 10; j++){
-
-        cout << array[i][j];
+        std:: cout << array[i][j];
         if (j != 9){
-            cout << " ";
+            std::cout << " ";
         }
 
     }
-    cout << "\n";
+    std::cout << "\n";
 }
 } 
