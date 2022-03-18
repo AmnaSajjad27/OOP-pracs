b6cef7e596ccd3460c356119773d87782841259e
Amna Sajjad <a1824665@adelaide.edu.au>
2022-03-18 11:14:25 +1030

    q2pracecam

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -41,8 +41,8 @@ int sum_if_a_palindrome(int integers[], int length){
     return sum;
 }
 
-int sum_elements(int integers[], int length){
-    extern bool is_a_palindrome(int integers[], int length);
-    extern int sum_if_a_palindrome(int integers[], int length);
+//int sum_elements(int integers[], int length){
+ //   extern bool is_a_palindrome(int integers[], int length);
+  //  extern int sum_if_a_palindrome(int integers[], int length);
 
-}
+//}
