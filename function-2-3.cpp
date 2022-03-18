d466c3c4652f6bf6991a87c33f24a0173c779eff
Amna Sajjad <a1824665@adelaide.edu.au>
2022-03-18 11:00:17 +1030

    q2pracexam

---
+++ function-2-3.cpp
@@ -0,0 +1,48 @@
+# include <iostream>
+
+// funcitons that sums array if they are palindrome array
+
+
+// function that checks if it is a palindrome array or not 
+bool is_a_palindrome(int integers[], int length){
+
+// fan array if it reads the same both ways 
+
+bool yes_or_no = 1;
+
+if (length < 0){
+    yes_or_no == -1;
+}
+
+for (int i = 0; i < length; i++){
+    if (integers[i] != integers[length-i-1]){
+        yes_or_no = 0;
+        std::cout << "-2"<< std::endl;
+    }
+}
+
+return yes_or_no;
+}
+
+
+
+
+// function that sums the array of it is a palindrome array
+int sum_if_a_palindrome(int integers[], int length){
+    extern bool is_a_palindrome(int integers[], int length);
+    double sum = 0;
+    if ( (is_a_palindrome(integers,length)) == 1){
+
+        for (int i = 0; i < length; i++){
+            sum = sum + integers[i];
+        }
+
+    }
+    return sum;
+}
+
+int sum_elements(int integers[], int length){
+    extern bool is_a_palindrome(int integers[], int length);
+    extern int sum_if_a_palindrome(int integers[], int length);
+
+}
---
+++ main-2-3.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+
+extern bool is_a_palindrome(int integers[], int length);
+extern int sum_if_a_palindrome(int integers[], int length);
+extern int sum_elements(int integers[], int length);
+int main(){
+
+    int integers[] = {1,2,3,3,2,1};
+    int length = 6;
+
+    std::cout << sum_elements(integers, length) << std::endl;
+
+}
