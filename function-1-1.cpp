65388be11086642906e8b45a7471eeab8ef004f2
Amna Sajjad <a1824665@adelaide.edu.au>
2022-03-23 16:54:17 +1030

    q1

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -4,12 +4,12 @@
 void copy_2d_strings(std::string first[][2], std::string second[][2], int n){
 
     for (int i = 0; i < n; i++){
-        for (int j = 0; j < 2; j++){
-            first[i][j] = second[i][j];
+        for (int j = 0; j < 2; j++){    
+            second[i][j] = first[i][j];
         }
     }
-    
-    
-    // std::cout << first << std::endl;
 
+   // for (int i = 0; i < n; i++){
+   //     for (int j = 0; j < 2; j++){
+    // std::cout << second[i][j] << std::endl;}}
 }
