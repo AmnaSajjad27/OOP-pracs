2db8a7a6422035913d48b3cfd985ada61b0069be
Amna Sajjad <a1824665@adelaide.edu.au>
2022-03-28 11:18:05 +1030

    q2

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -3,8 +3,12 @@
 extern void hexDigits(int *numbers,int length);
 
 int main(){
+
     int numbers[9] = {11,2,13,4,15,6,7,8,9};
     int length = 9;
 
     hexDigits(numbers,length);
+
+    delete[] numbers;
+    return 0;
 }
