1e6ee36b7d951dc18684b15d5890946f291c9b58
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2022-04-06 21:19:42 +0930

    wtf is happeninf>

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -9,12 +9,14 @@ int main(){
     P_1.setName("Mina");
     P_1.setSalary(19);
 
-    /* cout << P_1.getName() << P_1.getSalary() << endl;
+    P_1.setName("amna");
+
+
+/*      cout << P_1.getName() << P_1.getSalary() << endl;
     
     P_1.setName("amna");
     cout << P_1.getName() << P_1.getSalary() << endl;
- */
+  */
    return 0;
-
 
 }
