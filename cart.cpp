630d7a0dce8961795a7b09f769aeff084059ffca
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2022-04-08 09:18:29 +0930

    count0

---
+++ cart.cpp
@@ -0,0 +1,35 @@
+# include "meerkat.h"
+# include "cart.h"
+
+using namespace std;
+
+cart::cart(){
+    count = 0;
+    cats = new meerkat[4];
+}
+bool cart::addMeerkat(meerkat cat){
+    cats[count] = cat;
+    count++;
+    
+    if (count > 3){
+        return false;
+        cout << "oops cannot add meerkat";
+        count = 0;
+    }
+    return true;
+}
+
+void cart::printMeerkats(){
+    for (int i = 0; i <= 3; i++){
+    cout << cats[i].getName() << " " << cats[i].getAge() << "\n";
+    }
+}
+
+void cart::emptyCart(){
+    for (int i = 0; i < (count + 1); i++) {
+    //    cats[i]-> ~meerkat();
+    delete[] &(cats[i]);
+    }
+    count = 0;
+}
+
