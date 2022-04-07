1acbc50c18a819e6ca78dedd8ebb25f66dde7a7d
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2022-04-07 13:14:17 +0930

    commented out main

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 int main(){
+    /*
 
     person P_1;
     P_1.setName("Mina");
-    P_1.setSalary(19);
-
-
-     cout << P_1.getName() << P_1.getSalary() << endl;
-    
+    P_1.setSalary(7500);
+    cout << P_1.getName() << " " << P_1.getSalary() << endl;
+    // change name and salary
     P_1.setName("amna");
-    cout << P_1.getName() << P_1.getSalary() << endl;
-
+    P_1.setSalary(750);
+    cout << P_1.getName() << " " << P_1.getSalary() << endl;
+*/
    return 0;
 
 }
