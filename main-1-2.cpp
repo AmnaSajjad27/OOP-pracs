9ed23786f11a9fa3b744630d6bbfb433c514ba48
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2022-04-07 11:35:33 +0930

    maybe...

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -9,14 +9,12 @@ int main(){
     P_1.setName("Mina");
     P_1.setSalary(19);
 
-    P_1.setName("amna");
 
-
-/*      cout << P_1.getName() << P_1.getSalary() << endl;
+     cout << P_1.getName() << P_1.getSalary() << endl;
     
     P_1.setName("amna");
     cout << P_1.getName() << P_1.getSalary() << endl;
-  */
+
    return 0;
 
 }
