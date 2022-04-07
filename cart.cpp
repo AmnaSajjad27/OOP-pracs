2ab4fd27e007db2b93a1c29ed7228c9dd0da0575
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2022-04-07 21:08:35 +0930

    Add files via upload

--- cart.cpp
+++ cart.cpp
@@ -12,9 +12,8 @@ bool cart::addMeerkat(meerkat cat){
     count++;
     
     if (count > 3){
+        return 0;
          cout << "oops cannot add meerkat";
-         return 0;
-         break;
     }
     return 1;
 }
